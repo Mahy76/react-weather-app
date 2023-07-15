1501f7b8357fe4aa632b421660406481e4af195e
Mahy76 <mahsanosrati76@gmail.com>
2023-07-15 22:55:53 +0330

    fixed the code

--- src/Search.js
+++ src/Search.js
@@ -1,76 +1,100 @@
 import React, { useState } from "react";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import "./Search.css";
+
 export default function Search() {
-  let [city, setCity] = useState("");
-  let [temp, setTemp] = useState("");
+  const [city, setCity] = useState("");
+  const [temp, setTemp] = useState(null);
 
   function getCity(event) {
-    event.preventDefault();
     setCity(event.target.value);
   }
+
   function showTemp(event) {
     event.preventDefault();
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+
     axios.get(url).then(function (response) {
       setTemp(response.data);
-      console.log(response.data);
     });
   }
 
   return (
-    <div className="search">
-      <form onSubmit={showTemp}>
-        <label htmlFor="search">Search for a city:</label>
-        <br />
-        <input type="search" id="search" onChange={getCity} />
-        <button type="submit">Search</button>
-      </form>
-      <ul>
-        {temp && temp.weather && (
-          <div>
-            <li>Temperature: {Math.round(temp.main.temp)}°C</li>
-            <li>Description: {temp.weather[0].description}</li>
+    <div className="container">
+      <div className="kinky-weather-wrapper">
+        <div className="kinky-weather">
+          {temp && temp.weather && (
+            <div className="overview">
+              <h1>{temp.name}</h1>
+              <ul>
+                <li>Last updated: {new Date().toLocaleString()}</li>
+                <li>{temp.weather[0].description}</li>
+              </ul>
+            </div>
+          )}
 
-            <li>Humidity: {temp.main.humidity}%</li>
-            <li>Wind: {temp.wind.speed}</li>
-
-            <img
-              src={`http://openweathermap.org/img/wn/${temp.weather[0].icon}.png`}
-              alt="Weather Icon"
-            />
+          <div className="row">
+            <div className="col-6">
+              {temp && temp.weather && (
+                <div className="d-flex temp-image weather-temperature">
+                  <img
+                    src={`http://openweathermap.org/img/wn/${temp.weather[0].icon}.png`}
+                    alt="Weather Icon"
+                  />
+                  <div className="d-flex">
+                    <strong>{Math.round(temp.main.temp)}°C</strong>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
-        )}
-      </ul>
+
+          <div className="search">
+            <form className="mb-3" onSubmit={showTemp}>
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    id="search"
+                    placeholder="Type a city..."
+                    className="form-control"
+                    autoComplete="off"
+                    onChange={getCity}
+                  />
+                </div>
+                <div className="col-3">
+                  <button
+                    type="submit"
+                    value="search"
+                    className="btn btn-primary w-100"
+                  >
+                    Search
+                  </button>
+                </div>
+              </div>
+            </form>
+
+            {temp && temp.weather && (
+              <ul>
+                <li>Humidity: {temp.main.humidity}%</li>
+                <li>Wind: {temp.wind.speed}</li>
+              </ul>
+            )}
+
+            <small>
+              <a
+                href="https://github.com/Mahy76/kinky-weather"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Open-source code
+              </a>{" "}
+              by Mahsa Nosrati
+            </small>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-
-
-
-<div className="Search">
-      <form className="mb-3" onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city..."
-              className="form-control"
-              autoComplete="off"
-              value={city}
-              onChange={(event) => setCity(event.target.value)}
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-    </div>
-  );
