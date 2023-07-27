ca2ceaf3437d9bdddf3db3c1d5d5a1548ed7c68d
Mahy76 <mahsanosrati76@gmail.com>
2023-07-27 21:36:26 +0330

    added Date and time component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Search />
+        <Search defaultCity="New York" />
+        <footer>
+          {" "}
+          <small>
+            <a
+              href="https://github.com/Mahy76/react-weather-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Open-source code
+            </a>
+            <span>by Mahsa Nosrati</span>
+          </small>
+        </footer>
       </header>
     </div>
   );
--- src/Search.js
+++ src/Search.js
@@ -2,113 +2,115 @@ import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import WeatherIcon from "react-icons-weather";
+import FormattedDate from "./FormattedDate";
 import "./Search.css";
 
-export default function Search() {
+export default function Search(props) {
   const [city, setCity] = useState("");
-  const [temp, setTemp] = useState(null);
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
+  function setTemp(response) {
+    setWeatherData({
+      ready: true,
+      data: new DataTransfer(response.data.dt * 1000),
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      iconUrl: response.data,
+    });
+  }
   function handleInputChange(event) {
     setCity(event.target.value);
   }
 
   function handleFormSubmit(event) {
     event.preventDefault();
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
-
-    axios
-      .get(url)
-      .then(function (response) {
-        setTemp(response.data);
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.error("Error fetching weather data:", error);
-      });
   }
-
-  return (
-    <div className="container">
-      <div className="kinky-weather-wrapper">
-        <div className="kinky-weather">
-          <div className="search">
-            <form className="mb-3" onSubmit={handleFormSubmit}>
-              <div className="row">
-                <div className="col-9">
-                  <input
-                    type="search"
-                    id="search"
-                    placeholder="Type a city..."
-                    className="form-control"
-                    autoComplete="off"
-                    value={city}
-                    onChange={handleInputChange}
-                  />
+  if (weatherData.ready) {
+    return (
+      <div className="container">
+        <div className="kinky-weather-wrapper">
+          <div className="kinky-weather">
+            <div className="search">
+              <form className="mb-3" onSubmit={handleFormSubmit}>
+                <div className="row">
+                  <div className="col-9">
+                    <input
+                      type="search"
+                      id="search"
+                      placeholder="Type a city..."
+                      className="form-control"
+                      autoComplete="off"
+                      value={weatherData.city}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div className="col-3">
+                    <button
+                      type="submit"
+                      value="search"
+                      className="btn btn-info w-100"
+                    >
+                      Search
+                    </button>
+                  </div>
                 </div>
-                <div className="col-3">
-                  <button
-                    type="submit"
-                    value="search"
-                    className="btn btn-info w-100"
-                  >
-                    Search
-                  </button>
-                </div>
-              </div>
-            </form>
-            {temp && (
-              <div className="overview">
-                <h1>{city}</h1>
-                <div className="container">
-                  <div className="row">
-                    <div className="col-3">
-                      <div>
-                        <h2>{Math.round(temp.main.temp)}</h2>°C
+              </form>
+              {temp && (
+                <div className="overview">
+                  <h1>{weatherData.city}</h1>
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-3">
+                        <div>
+                          <h2>{Math.round(weatherData.temp)}</h2>°C
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-6">
-                      <div className="  weather-temperature">
-                        {temp.weather[0] && (
+                      <div className="col-6">
+                        <div className="  weather-temperature">
                           <div>
                             <WeatherIcon
                               name="owm"
-                              iconId={temp.weather[0].id}
+                              iconId={weatherData.id}
                               flip="horizontal"
                               rotate="90"
                               className="weather-icon"
                             />
                           </div>
-                        )}
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
 
-                <ul className="mt-3">
-                  <li>Last updated: {new Date().toLocaleString()}</li>
-                  <li>
-                    <strong>Description: {temp.weather[0].description}</strong>
-                  </li>
-                </ul>
-                <ul>
-                  <li>Humidity: {temp.main.humidity}%</li>
-                  <li>Wind: {temp.wind?.speed}</li>
-                </ul>
-              </div>
-            )}
+                  <ul className="mt-3">
+                    <li>
+                      Last updated: <FormattedDate date={weatherData.date} />
+                    </li>
+                    <li>
+                      <strong className="text-capitalize">
+                        Description: {weatherData.description}
+                      </strong>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li>Humidity: {weatherData.humidity}%</li>
+                    <li>Wind: {weatherData.wind}</li>
+                  </ul>
+                </div>
+              )}
+            </div>
           </div>
         </div>
-        <small>
-          <a
-            href="https://github.com/Mahy76/react-weather-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Open-source code
-          </a>
-          <span>by Mahsa Nosrati</span>
-        </small>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "3a94f3778290bfeee61278505dbbe51";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}d&units=metric`;
+
+    axios.get(url).then(setTemp);
+
+    return "Loading...";
+  }
 }
