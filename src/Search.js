2a5e78a1ebf4a5499c8919133c0fbfa3068216d6
Mahy76 <mahsanosrati76@gmail.com>
2023-07-28 11:28:17 +0330

    Changed the CSS name and improved the Weather.js

--- src/Search.js
+++ src/Search.js
@@ -1,73 +1,65 @@
 import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import axios from "axios";
 import WeatherInfo from "./Info";
-import "./Search.css";
+import axios from "axios";
+import "./Weather.css";
 
-export default function Search(props) {
-  const [city, setCity] = useState(props.defaultCity);
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-  function setTemp(response) {
-    console.log(response.data);
+  function handleResponse(response) {
     setWeatherData({
       ready: true,
-      data: new DataTransfer(response.data.dt * 1000),
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      wind: response.data.wind.speed,
-      city: response.data.name,
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
     });
   }
-  function search() {
-    const apiKey = "3a94f3778290bfeee61278505dbbe51";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}d&units=metric`;
 
-    axios.get(url).then(setTemp);
-  }
-  function handleFormSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
     search();
   }
-  function handleInputChange(event) {
+
+  function handleCityChange(event) {
     setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   if (weatherData.ready) {
     return (
-      <div className="container">
-        <div className="kinky-weather-wrapper">
-          <div className="kinky-weather">
-            <div className="search">
-              <form className="mb-3" onSubmit={handleFormSubmit}>
-                <div className="row">
-                  <div className="col-9">
-                    <input
-                      type="search"
-                      id="search"
-                      placeholder="Type a city..."
-                      className="form-control"
-                      autoComplete="off"
-                      onChange={handleInputChange}
-                    />
-                  </div>
-                  <div className="col-3">
-                    <button
-                      type="submit"
-                      value="search"
-                      className="btn btn-info w-100"
-                    >
-                      Search
-                    </button>
-                  </div>
-                </div>
-              </form>
-              <WeatherInfo data={weatherData} />
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-        </div>
+        </form>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
