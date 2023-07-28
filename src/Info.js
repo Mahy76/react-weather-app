0a4ca987d2556ee966066feda40146dfbab0c048
Mahy76 <mahsanosrati76@gmail.com>
2023-07-28 11:14:57 +0330

    changed the structure of Info showing

--- src/Info.js
+++ src/Info.js
@@ -1,45 +1,36 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-import WeatherIcon from "react-icons-weather";
 import WeatherIcon from "./WeatherIcon";
+
 export default function Info(props) {
   return (
-    <div className="overview">
+    <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-      <div className="container">
-        <div className="row">
-          <div className="col-3">
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="d-flex">
             <div>
-              <h2>{Math.round(props.data.temp)}</h2>°C
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
-          </div>
-          <div className="col-6">
-            <div className="  weather-temperature">
-              <div className="float-left">
-                <WeatherIcon
-                  code={props.data.icon}
-                  alt={props.data.description}
-                />
-              </div>
+
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
+        </div>
       </div>
-
-      <ul className="mt-3">
-        <li>
-          Last updated: <FormattedDate date={props.data.date} />
-        </li>
-        <li>
-          <strong className="text-capitalize">
-            Description: {props.data.description}
-          </strong>
-        </li>
-      </ul>
-      <ul>
-        <li>Humidity: {props.data.humidity}%</li>
-        <li>Wind: {props.data.wind}</li>
-      </ul>
     </div>
   );
 }
