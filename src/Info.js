dafd7489c0ffce59cd11c23b633d93ab79e1b6ee
Mahy76 <mahsanosrati76@gmail.com>
2023-07-29 09:37:49 +0330

    fixed the useState error

--- src/Info.js
+++ src/Info.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function Info(props) {
   return (
@@ -17,6 +18,9 @@ export default function Info(props) {
           <div className="d-flex">
             <div>
               <WeatherIcon code={props.data.icon} size={52} />
+            </div>
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
